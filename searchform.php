651a6d43b799699d2002d44dcd3f01d4ab82cbcc
owt5008137@live.com <owt5008137@live.com>
2013-12-16 20:26:51 +0800

    fix search

--- searchform.php
+++ searchform.php
@@ -1,12 +1,10 @@
 <form method="get" id="searchform" action="<?php echo home_url(); ?>/">
-<input type="text" value="Search..." onblur="if (this.value == '')
-
-{this.value = 'Search...';}"
-
-onfocus="if (this.value == 'Search...')
-
-{this.value = '';}" name="s" id="s" />
-
-<input type="submit" id="searchsubmit" value="Search">
-
+<div class="input-group">
+  <span class="input-group-btn">
+  <button type="submit" class="btn btn-default">
+  	<span class="glyphicon glyphicon-search"></span>
+  </button>
+  </span>
+  <input type="text" class="form-control" placeholder="Search..." name="s" id="s" />
+</div>
 </form>
