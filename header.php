dc5c87767b67a6718cb6ed699ce2d77369c1e345
owt5008137@live.com <owt5008137@live.com>
2013-12-17 19:43:21 +0800

    use bootstrap menu

--- header.php
+++ header.php
@@ -74,29 +74,31 @@
 
     <div id="masthead">
     	<?php if ( 'above' == $wp_distinctionpp_options['header_menu_position'] ) { ?>
-            <div id="header-menu">
-
-                <div id="access" role="navigation">
-
-                    <?php /*  Allow screen readers / text browsers to skip the navigation menu and get right to the good stuff */ ?>
-                    <div class="skip-link screen-reader-text">
-                        <a href="#content" title="<?php esc_attr_e( 'Skip to content', 'wp_distinctionpp' ); ?>"><?php _e( 'Skip to content', 'wp_distinctionpp' ); ?></a>
-                    </div>
-
-                    <div class="menu-header">
-						<?php /* Our navigation menu.  If one isn't filled out, wp_nav_menu falls back to wp_page_menu.  The menu assiged to the primary position is the one used.  If none is assigned, the menu with the lowest ID is used.  */ ?>
-                        <?php wp_nav_menu( array( 'container' => '', 'theme_location' => 'primary' ) ); ?>
-
-                        <?php if ( 'true' == $wp_distinctionpp_options['display_menu_search_form'] ) { // Enabled by default ?>
-						<div id="search-form"><?php get_search_form(); ?></div>
-
-						<?php } ?>
-
-                    </div><!-- .menu-header -->
-
-                </div><!-- #access -->
-
-            </div><!-- #header-menu -->
+	    <nav id="header-menu" class="navbar navbar-inverse navbar-right" role="navigation">
+	        <ul id="nav" class="nav navbar-nav text-center"><?php
+	        /* Our navigation menu.  If one isn't filled out, wp_nav_menu falls back to wp_page_menu.  The menu assiged to the primary position is the one used.  If none is assigned, the menu with the lowest ID is used.  */
+				global $wp_distinctionpp_options;
+				if ( has_nav_menu( 'primary' ) ) { // If a nav menu named 'nav-header' is configured, output it
+					wp_nav_menu( array(
+						'container' => '',
+						'fallback_cb' => '',
+						'depth' => $wp_distinctionpp_options['header_menu_depth'],
+						'theme_location' => 'primary'
+					));
+				} else { // otherwise, output the top-level hierarchy list of pages
+					if ( get_option( 'show_on_front' ) == 'posts' ) { // if posts, and not a static page, are being used as the site home page, display a link to HOME ?>
+				<li><a id="navhome" href="<?php echo home_url(); ?>">Home</a></li><?php
+					}
+					wp_list_pages( array( 'depth' => $wp_distinctionpp_options['header_menu_depth'], 'sort_column' => 'menu_order', 'title_li' => '' ) );
+					// depth=1 indicates that only the top-level hierarchy of pages (i.e. no child pages) will be displayed
+					// sort_column=menu_order indicates that the pages will be sorted as defined by the user in the Pages administration panel
+					// title_li= (blank) indicates that the list will not be wrapped in <ul> tags, and the <li> tags will not be given a title
+					// NOTE: list items are set to overflow:hidden. Long page titles will be cut off, but the full Page Title will display in the tooltip
+				}
+				if ( 'true' == $wp_distinctionpp_options['display_menu_search_form'] ) { // Enabled by default ?>
+				<li><div id="search-form" class="navbar-right"><?php get_search_form(); ?></div></li><?php
+				} ?></ul>
+	    </nav>
        <?php } ?>
         <div id="branding" role="banner">
 
@@ -122,54 +124,31 @@
 
       	</div><!-- #branding -->
        	<?php if ( 'below' == $wp_distinctionpp_options['header_menu_position'] ) { ?>
-            <div id="header-menu">
-
-                <div id="access" role="navigation">
-
-                    <?php /*  Allow screen readers / text browsers to skip the navigation menu and get right to the good stuff */ ?>
-                    <div class="skip-link screen-reader-text">
-                        <a href="#content" title="<?php esc_attr_e( 'Skip to content', 'distinctionpp' ); ?>"><?php _e( 'Skip to content', 'wp_distinctionpp' ); ?></a>
-                    </div>
-
-                    <div class="menu-header">
-
-						<?php /* Our navigation menu.  If one isn't filled out, wp_nav_menu falls back to wp_page_menu.  The menu assiged to the primary position is the one used.  If none is assigned, the menu with the lowest ID is used.  */ ?>
-                        <?php //wp_nav_menu( array( 'container' => '', 'theme_location' => 'primary' ) ); ?>
-
-                        <?php
-						global $wp_distinctionpp_options;
-						if ( has_nav_menu( 'primary' ) ) { // If a nav menu named 'nav-header' is configured, output it
-							wp_nav_menu( array(
-								'container' => '',
-								'fallback_cb' => '',
-								'depth' => $wp_distinctionpp_options['header_menu_depth'],
-								'theme_location' => 'primary'
-							) );
-							} else { // otherwise, output the top-level hierarchy list of pages ?>
-								<ul class="nav navbar-nav">
-									<?php if ( get_option( 'show_on_front' ) == 'posts' ) { // if posts, and not a static page, are being used as the site home page, display a link to HOME ?>
-										<li><a id="navhome" href="<?php echo home_url(); ?>">Home</a></li>
-									<?php }
-									wp_list_pages( array( 'depth' => $wp_distinctionpp_options['header_menu_depth'], 'sort_column' => 'menu_order', 'title_li' => '' ) );
-									// depth=1 indicates that only the top-level hierarchy of pages (i.e. no child pages) will be displayed
-									// sort_column=menu_order indicates that the pages will be sorted as defined by the user in the Pages administration panel
-									// title_li= (blank) indicates that the list will not be wrapped in <ul> tags, and the <li> tags will not be given a title
-									// NOTE: list items are set to overflow:hidden. Long page titles will be cut off, but the full Page Title will display in the tooltip ?>
-								</ul>
-						<?php } ?>
-
-                    	<?php if ( 'true' == $wp_distinctionpp_options['display_menu_search_form'] ) { // Enabled by default ?>
-
-						<div id="search-form"><?php get_search_form(); ?></div>
-
-						<?php } ?>
-
-
-                    </div><!-- .menu-header -->
-
-                </div><!-- #access -->
-
-            </div><!-- #header-menu -->
+	    <nav id="header-menu" class="navbar navbar-inverse navbar-right" role="navigation">
+	        <ul id="nav" class="nav navbar-nav text-center"><?php
+	        /* Our navigation menu.  If one isn't filled out, wp_nav_menu falls back to wp_page_menu.  The menu assiged to the primary position is the one used.  If none is assigned, the menu with the lowest ID is used.  */
+				global $wp_distinctionpp_options;
+				if ( has_nav_menu( 'primary' ) ) { // If a nav menu named 'nav-header' is configured, output it
+					wp_nav_menu( array(
+						'container' => '',
+						'fallback_cb' => '',
+						'depth' => $wp_distinctionpp_options['header_menu_depth'],
+						'theme_location' => 'primary'
+					));
+				} else { // otherwise, output the top-level hierarchy list of pages
+					if ( get_option( 'show_on_front' ) == 'posts' ) { // if posts, and not a static page, are being used as the site home page, display a link to HOME ?>
+				<li><a id="navhome" href="<?php echo home_url(); ?>">Home</a></li><?php
+					}
+					wp_list_pages( array( 'depth' => $wp_distinctionpp_options['header_menu_depth'], 'sort_column' => 'menu_order', 'title_li' => '' ) );
+					// depth=1 indicates that only the top-level hierarchy of pages (i.e. no child pages) will be displayed
+					// sort_column=menu_order indicates that the pages will be sorted as defined by the user in the Pages administration panel
+					// title_li= (blank) indicates that the list will not be wrapped in <ul> tags, and the <li> tags will not be given a title
+					// NOTE: list items are set to overflow:hidden. Long page titles will be cut off, but the full Page Title will display in the tooltip
+				}
+				if ( 'true' == $wp_distinctionpp_options['display_menu_search_form'] ) { // Enabled by default ?>
+				<li><div id="search-form" class="navbar-right"><?php get_search_form(); ?></div></li><?php
+				} ?></ul>
+	    </nav>
        <?php } ?>
     </div><!-- #masthead -->
 
