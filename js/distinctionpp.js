88edbed8a6eb54d0376cf2859de84e33134a25ac
owent <owt5008137@live.com>
2016-07-11 16:50:11 +0800

    调整完毕

--- js/distinctionpp.js
+++ js/distinctionpp.js
@@ -1,9 +1,9 @@
-jQuery(document).ready(function(){
+jQuery(document).ready(function () {
     var is_transition_enabled = false;
     if (window.Modernizr && window.Modernizr.csstransitions) {
         is_transition_enabled = true;
     }
-    
+
     // 瀑布流布局
     jQuery("#post-wrapper").SimpleFliud({
         // 数值设置
@@ -11,24 +11,24 @@ jQuery(document).ready(function(){
         margin_row: 15,
         margin_col: 15,
         duration: 500, // 动画过度时间
-        
+
         // 选择器设置
         panel: ">div.post_fliud_panel",            // 内部面板选择器(默认为div)
         wrapper: "#content",     // 包装器选择器，默认为容器选择器
 
         // 其他设置
         auto_width: true,         // 自动调整宽度使之填满整个panel(默认为false)
-        action_type: is_transition_enabled? "immediately": "animation"  // 变换类型,当浏览器支持CSS3时,使用CSS3动画
+        action_type: is_transition_enabled ? "immediately" : "animation"  // 变换类型,当浏览器支持CSS3时,使用CSS3动画
     });
 
     // Menu
-    jQuery("#widget-area-toggle-btn").click(function(){
-        jQuery(".widget-area").slideToggle();
+    jQuery("#widget-area-toggle-btn").click(function () {
+        jQuery(".widget-area>aside>.xoxo").slideToggle();
     });
-    jQuery(".widget-area").hide();
-    
+    jQuery(".widget-area>aside>.xoxo").hide();
+
     if (is_transition_enabled) {
-        jQuery("#post-wrapper").css({"transition": "all .5s ease-in-out"})
-        jQuery("#post-wrapper>div.post_fliud_panel").css({"transition": "all .5s ease-in-out"})
+        jQuery("#post-wrapper").css({ "transition": "all .5s ease-in-out" })
+        jQuery("#post-wrapper>div.post_fliud_panel").css({ "transition": "all .5s ease-in-out" })
     }
 });
